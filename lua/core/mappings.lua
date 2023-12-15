b5477b79684fc68dea3a93627a422ac53546e6e1
Khnykin Artem <113325581+Khnykin-Artem@users.noreply.github.com>
2023-12-15 19:01:00 +0300

    edit treesitter.lua

---
+++ lua/core/mappings.lua
@@ -0,0 +1,42 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+-- Navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
+
+-- Splits
+vim.keymap.set('n', '|', ':vsplit<CR>')
+vim.keymap.set('n', '\\', ':split<CR>')
+
+-- Other
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
+vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Tabs
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+
+-- Lsp 
+vim.keymap.set('n', '<leader>lD', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.setloclist)
+
+-- Diffview
+vim.keymap.set('n', '<leader>l', ':DiffviewLog<CR>')
+vim.keymap.set('n', '<leader>lo', ':DiffviewOpen<CR>')
+vim.keymap.set('n', '<leader>lc', ':DiffviewClose<CR>')
+
+-- Flog
+vim.keymap.set('n', '<leader>fg', ':Flog<CR>')
