31d108bbc80aa5d01a89d5f81c4e19f59120f48d
Khnykin-Artem <k98030118@gmail.com>
2024-01-07 14:46:32 +0300

    treesitter for c

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "typescript", "lua", "javascript", "css", "html", "json", "scss", "gitignore", "comment" },
+  ensure_installed = { "typescript", "lua", "javascript", "css", "html", "json", "scss", "gitignore", "comment", "c" },
 
   sync_install = false,
   auto_install = true,
